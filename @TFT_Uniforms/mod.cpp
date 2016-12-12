0ce9517af752163fea8cd874e28f794d0804180c
Pawel W <pawel.wawron@gmail.com>
2016-12-12 15:08:40 +0100

    Updates to a release
    + new key
    + new mod.paa
    + changes in mod.cpp

--- @TFT_Uniforms/mod.cpp
+++ @TFT_Uniforms/mod.cpp
@@ -1,14 +1,14 @@
-name = "TFT8 Uniforms";     /// Name of your mod
-picture = "overview.paa";   /// Picture displayed /Optimal size is 2048x1024, other sizes work too
+name = "TFT8 Uniforms";
+picture = "overview.paa";
 author = "YoursTruly, FitzGerald";
-logoSmall = "mod.paa";  /// Display next to the item added by the mod
-logo = "mod.paa";   /// Logo displayed in the main menu
-logoOver = "TFT8.paa";  /// When the mouse is over, in the main menu
+logoSmall = "mod.paa";
+logo = "mod.paa";
+logoOver = "TFT8.paa";
 tooltip = "TFT8 Uniforms";
-tooltipOwned = "TFT8 Uniforms Installed";  /// Tool tip displayed when the mouse is left over, in the main menu
-overview = "Come Join us in ARMA 3 at modded.tft8.com:2303.<br/>    on Teamspeak at:TS3.TFT8.com<br/>    and on the web at http://tft8.com.<br/><br/><t color='#7C7C7C'>This content is under APL-SA License.<t />";  /// Overview text, displayed from the extension menu
-overviewPicture = "overview.paa"; // the 3:2 format picture on the overview page in the expansions menu
-overviewText = "Task Force Tempor 8 Uniforms Mod"; //
+tooltipOwned = "TFT8 Uniforms";
+overview = "Come Join us in ARMA 3 at modded.tft8.com:2303.<br/>    on Teamspeak at:TS3.TFT8.com<br/>    and on the web at http://tft8.com.<br/><br/><t color='#7C7C7C'>This content is under APL-SA License.<t />";
+overviewPicture = "overview.paa";
+overviewText = "Task Force Tempor 8 Uniforms Mod";
 overviewFootnote = "<br/><t color='#7C7C7C'>This content is under APL-SA License.<t />";
-actionName = "TFT8 website";  //Names the button that links to the website URL accessed from the ex
-action = "https://tft8.com";  /// Website URL, that can accessed from the expansions menu
+actionName = "website";
+action = "https://github.com/TFT8/Uniforms";
